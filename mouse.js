13332927787bd5e748c8c2132dcc0d26e98e0ba3
Mikepiano97 <thold.marketing@gmail.com>
2019-07-20 22:28:23 +0700

    Mouse Jumpping Method Done

--- mouse.js
+++ mouse.js
@@ -4,10 +4,14 @@ function Mouse(name) {
 
 Mouse.prototype.sleep = function() {
 	console.log('Mouse is sleeping!');
-};
+}
 
 Mouse.prototype.run = function() {
 	console.log('Mouse is running...');
-};
+}
+
+Mouse.prototype.jump = function() {
+	console.log('Jumping mouse is an interesting idea!');
+}
 
 module.exports = Mouse;
